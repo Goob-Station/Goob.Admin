50525ae42153eafa92d8b80a3b2096d71590d9d6
Aidenkrz <28298836+Aidenkrz@users.noreply.github.com>
2026-07-24 17:56:59 -0500

    IP/HWID Unban

--- SS14.Admin/Pages/Bans/Hits.cshtml.cs
+++ SS14.Admin/Pages/Bans/Hits.cshtml.cs
@@ -8,6 +8,7 @@ using SS14.Admin.Pages.Connections;
 
 namespace SS14.Admin.Pages.Bans;
 
+[ValidateAntiForgeryToken]
 public class Hits : PageModel
 {
     private readonly PostgresServerDbContext _dbContext;
@@ -64,5 +65,61 @@ public class Hits : PageModel
             AllRouteData);
 
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostUnbanIpAsync(int ban)
+    {
+        if (!User.IsInRole("BAN"))
+            return Forbid();
+
+        var banEntity = await _dbContext.Ban
+            .Include(b => b.Addresses)
+            .SingleOrDefaultAsync(b => b.Id == ban);
+
+        if (banEntity == null)
+        {
+            TempData.SetStatusError("Unable to find ban");
+            return RedirectToPage(new { ban });
+        }
+
+        if (banEntity.Addresses is not { Count: > 0 })
+        {
+            TempData.SetStatusError("Ban has no IP address to unban");
+            return RedirectToPage(new { ban });
+        }
+
+        _dbContext.BanAddress.RemoveRange(banEntity.Addresses);
+        await _dbContext.SaveChangesAsync();
+
+        TempData.SetStatusInformation("IP address removed from ban");
+        return RedirectToPage(new { ban });
+    }
+
+    public async Task<IActionResult> OnPostUnbanHwidAsync(int ban)
+    {
+        if (!User.IsInRole("BAN"))
+            return Forbid();
+
+        var banEntity = await _dbContext.Ban
+            .Include(b => b.Hwids)
+            .SingleOrDefaultAsync(b => b.Id == ban);
+
+        if (banEntity == null)
+        {
+            TempData.SetStatusError("Unable to find ban");
+            return RedirectToPage(new { ban });
+        }
+
+        if (banEntity.Hwids is not { Count: > 0 })
+        {
+            TempData.SetStatusError("Ban has no HWID to unban");
+            return RedirectToPage(new { ban });
+        }
+
+        _dbContext.BanHwid.RemoveRange(banEntity.Hwids);
+        await _dbContext.SaveChangesAsync();
+
+        TempData.SetStatusInformation("HWID removed from ban");
+        return RedirectToPage(new { ban });
     }
 }
